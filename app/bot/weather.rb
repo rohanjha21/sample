fabb58e76f93f4831d029ada7130789fea574187
Rohan Jha <rohanjha21@yahoo.com>
2016-12-01 00:53:57 -0500

    zipcode

--- app/bot/weather.rb
+++ app/bot/weather.rb
@@ -1,6 +1,8 @@
 # bot.rb
 require 'facebook/messenger'
 include Facebook::Messenger
+require 'net/http'
+require 'json'
 
 # Facebook::Messenger.configure do |config|
 #   # config.access_token = ENV['PAGE_ACCESS_TOKEN']
@@ -9,12 +11,37 @@ include Facebook::Messenger
 # end
 
 Bot.on :message do |message|
-  message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
-  message.sender      # => { 'id' => '1008372609250235' }
-  message.seq         # => 73
-  message.sent_at     # => 2016-04-22 21:30:36 +0200
-  message.text        # => 'Hello, bot!'
-  message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
+  
+	case message.text
+  
+	when /zipcode/i
+  		zipcode = message.text.split
+  	
+  		if zipcode.length == 2 and zipcode[1].length == 5
+  			user_zipcode = zipcode[1]
 
-  message.reply(text: 'Fuck you Janet!')
+  			Bot.deliver(
+        		recipient: message.sender,
+       			message: {
+          			text: "This is your zipcode: #{user_zipcode}."
+        		}
+        	)
+  		else
+  			Bot.deliver(
+        		recipient: message.sender,
+       			message: {
+          			text: "Sorry, we couldn't catch that. Try type: zipcode *your 5 digit zipcode*"
+        		}
+        	)
+    	end 
+	end
+
+	else
+		Bot.deliver(
+        	recipient: message.sender,
+       		message: {
+          		text: "Fuck you Janet."
+        	}
+        )
+    end
 end
