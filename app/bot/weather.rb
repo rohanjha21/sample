a7552bdef28ea1e9d75de8cc0d5ce154ba1017a0
Rohan Jha <rohanjha21@yahoo.com>
2016-12-01 20:36:11 -0500

    weather

--- app/bot/weather.rb
+++ app/bot/weather.rb
@@ -1,47 +1,48 @@
-# bot.rb
 require 'facebook/messenger'
-include Facebook::Messenger
 require 'net/http'
 require 'json'
 
-# Facebook::Messenger.configure do |config|
-#   # config.access_token = ENV['PAGE_ACCESS_TOKEN']
-#   # config.verify_token = ENV['VERIFY_TOKEN']
-#   config.provider = Configuration::Providers::Environment.new
-# end
+include Facebook::Messenger
 
 Bot.on :message do |message|
-  
-	case message.text
-  
-	when /zipcode/i
-  		zipcode = message.text.split
-  	
-  		if zipcode.length == 2 and zipcode[1].length == 5
-  			user_zipcode = zipcode[1]
+  message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
+  message.sender      # => { 'id' => '1008372609250235' }
+  message.seq         # => 73
+  message.sent_at     # => 2016-04-22 21:30:36 +0200
+  message.text        # => 'Hello, bot!'
+  message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
 
-  			Bot.deliver(
-        		recipient: message.sender,
-       			message: {
-          			text: "This is your zipcode: #{user_zipcode}."
-        		}
-        	)
-  		else
-  			Bot.deliver(
-        		recipient: message.sender,
-       			message: {
-          			text: "Sorry, we couldn't catch that. Try type: zipcode *your 5 digit zipcode*"
-        		}
-        	)
-    	end 
-	end
-
-	else
-		Bot.deliver(
-        	recipient: message.sender,
-       		message: {
-          		text: "Fuck you Janet."
-        	}
-        )
+  case message.text
+  when /get started/i
+    message.reply(text: 'Welcome to Weather the Weather! We\'ll give you clothing recommendations based on the weather in your current location.')
+  when /hello/i
+    message.reply(text: 'Hello, human!')
+  when /help/i
+    message.reply(text: 'We\'re working on it')
+  when /zipcode/i
+    zipcode = message.text.split
+    if zipcode.length == 2 and zipcode[1].length == 5
+    user_zipcode = zipcode[1]
+    # store zipcode in user model
+    message.reply(text: "Your zipcode, #{user_zipcode}, has been updated!")
+    else
+      message.reply(text: "Sorry we didn't get your zipcode. Try typing: zipcode *your zipcode*")
     end
+  when /wear/i
+    url = 'http://api.openweathermap.org/data/2.5/weather?zip=94040,us&APPID=60a63f39a6b259fc6aa363e5f0879ddf'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    data = JSON.parse(response)
+
+    message.reply(text: "You are at: #{data['name']}.")
+  else
+    message.reply(text: 'No idea what you\'re saying')
+  end
 end
+
+# Bot.on :postback do |postback|
+#   case postback.payload
+#   when /WELCOME_NEW_USER/i
+#     message.reply(text: 'Welcome to Weather the Weather! We\'ll give you clothing recommendations based on the weather in your current location.')
+#   end
+#  end
